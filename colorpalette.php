f81da15b843c4c42ae0363be69438588f1d343bb
Kharisma <kharisma@localhost.localdomain>
2019-08-22 17:09:43 -0400

    Created HTLM boilerplate for color palette.

---
+++ colorpalette.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang='en'>
+    <!-- Begin Head -->
+    <head>
+        <!-- Basic -->
+        <meta charset='UTF-8'>
+        <!--<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>-->
+        <meta name='viewport' content='width=device-width,initial-scale=1'>
+        <meta name='theme-color' content='[+hexColor]'>
+        
+        <!-- Description & SEO -->
+        <title>Color Palette | BKreative Media Solutions</title>
+        <meta name='keywords' content='color, palette' />
+        <meta name='description' content='Using JS, this webpage will display relevant color palettes' />
+        <meta name='google-site-verification' content='[+id]' />
+        <meta property='twitter:account_id' content='[+id]' />
+        <meta name='p:domain_verify' content='[+id]'/>
+        
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://www.bkreative.net/resources/colorpalette' />
+        <meta property='og:image' content='bkms_fblogo.png' />
+        <meta property='og:title' content='Color Palette | BKreative Media Solutions' />
+        <meta property='og:description' content='Using JS, this webpage will display relevant color palettes' />
+        <meta property='og:site_name' content='Color Palette | BKreative Media Solutions'/>
+        
+        <meta name='twitter:card' content='summary_large_image'>
+        <meta name='twitter:title' content='Color Palette | BKreative Media Solutions'>
+        <meta name='twitter:description' content='Using JS, this webpage will display relevant color palettes'>
+        <meta name='twitter:url' content='https://www.bkreative.net/resources/colorpalette'>
+        <meta name='twitter:site' content='[+twitterHandle]'>
+        <meta name='twitter:creator' content='[+twitterName]'>
+        <meta name='twitter:image' content='bkms_fblogo.png'>
+        
+        <!-- Apple Bookmarks -->
+        <link href='bkms_applelogo.png' rel='apple-touch-icon' />
+        <link href='bkms_applelogo.png' rel='apple-touch-icon' sizes='76x76' />
+        <link href='bkms_applelogo.png' rel='apple-touch-icon' sizes='120x120' />
+        <link href='bkms_applelogo.png' rel='apple-touch-icon' sizes='152x152' />
+        <link href='bkms_applelogo.png' rel='apple-touch-icon' sizes='180x180' />
+        <link href='bkms_applelogo.png' rel='icon' sizes='192x192' />
+        <link href='bkms_applelogo.png' rel='icon' sizes='128x128' />
+        <link rel='shortcut icon' href='favicon.ico' type='image/x-icon'>
+        
+        <!-- PHP Includes -->
+        <?php include_once('dynamic/analyticstracking.php'); ?>
+    </head>
+    <!-- End Head -->
+    
+    <!-- BODY -->
+    <body>
+        <!-- HEADER -->
+        <header>
+            <nav>
+            </nav>
+        </header>
+        <!-- !END HEADER -->
+        
+        <!-- SECTION -->
+        <section id="">
+        </section>
+        <!-- !END SECTION -->
+        
+        <!-- FOOTER -->
+        <footer>
+        </footer>
+        <!-- !END FOOTER -->
+        
+        <!-- Stylesheets & Web Fonts -->
+        <link href='[+bootstrap]' rel='stylesheet'>
+        <link href='[+bootstraptheme]' rel='stylesheet'>
+        
+        <link href='[+stylesheet]' rel='stylesheet' type='text/css'/>
+        
+        <link href='[+googlefont]' rel='stylesheet'>
+        <link href='[+googlefont]' rel='stylesheet'>
+        
+        <!-- JavScript -->
+        <script src='[+jsURL]'></script>
+        <script src='[+jsURL]'></script>
+    </body>
+    <!-- !END BODY -->
+</html>
